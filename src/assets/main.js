d539ed811b5d694b1bd5143c7f6bfe6f0365e948
priya tadi <priya95tadi@hotmail.com>
2017-11-21 11:06:40 +0100

    Added first function"setHiddenFields"

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,5 +5,11 @@ function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
 }
+function setHiddenFields(){
+    answer=Math.floor(Math.random()*10000);
+    while(answer.Value.length<4){
+        answer.Value += 0 + answer.Value.toString();
+    }
+}
 
 //implement new functions here
